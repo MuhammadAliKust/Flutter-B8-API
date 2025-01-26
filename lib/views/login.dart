b97b858c811a399894e20320d6c5013eab59acb4
Muhammad Ali Nizami <kustalics@gmail.com>
2025-01-26 12:36:54 +0500

    Auth Services Integration Done

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -20,7 +20,7 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: Text("Loginsf"),
       ),
       body: Column(
         children: [
